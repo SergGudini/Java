31c86e79f9863529150e9d0e887e14f0d5341ee6
DargunSU <loft998@yandex.ru>
2023-05-03 22:02:24 +0300

    Home work 1

--- src/Seminar_1/hw/hw1.java
+++ src/Seminar_1/hw/hw1.java
@@ -1,17 +1,18 @@
 package Seminar_1.hw;
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+
 public class hw1 {
     public static void main(String args[]) {
-
+        Scanner input = new Scanner(System.in);
         /*int[] array = {1, 2, 1, 2, -1, 1, 3, 1, 3, -1, 0};*/
+        System.out.println("Insert size array:");
+        int size = input.nextInt();
+
         int[] array = new int[11];
         int sum = 0, count = 0, inputCount = 0;
         int i = 0;
 
-        Scanner input = new Scanner(System.in);
+
         System.out.println("Insert array elements:");
         while(true){
             inputCount = input.nextInt();
